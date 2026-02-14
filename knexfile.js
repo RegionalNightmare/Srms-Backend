5b689538cce32938ffb0e0add5bcf2ee50b49d2e
RegionalNightmare <Fentonmalik3@gmail.com>
2026-02-14 13:38:12 -0500

    More updates

--- knexfile.js
+++ knexfile.js
@@ -9,11 +9,12 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-    },
-
-     ssl: {
+      ssl: {
     ca: fs.readFileSync(process.env.DB_CA_PATH), 
   },
+    },
+
+     
     migrations: { directory: './migrations' },
     seeds: { directory: './seeds' },
   },
