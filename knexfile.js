3cdbd0c0773d5c1ac7a17b16b044880067d7aabb
RegionalNightmare <Fentonmalik3@gmail.com>
2026-02-14 14:14:04 -0500

    Maybe this time?

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,28 @@
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
+
+const dbConnection = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: process.env.DB_SSL_CERT,
+    rejectUnauthorized: true
+  }
+};
 
 module.exports = {
   development: {
     client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT || 26569),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: {
-         ca: process.env.DB_SSL_CERT,
-        rejectUnauthorized: true
-  },
-    },
-
-     
+    connection: dbConnection,
     migrations: { directory: './migrations' },
     seeds: { directory: './seeds' },
   },
+  production: {
+    client: 'mysql2',
+    connection: dbConnection,
+    migrations: { directory: './migrations' },
+    seeds: { directory: './seeds' },
+  }
 };
