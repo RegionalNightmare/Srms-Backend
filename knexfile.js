a0345411ffc655bf9e62c95ea03bc420e165ec79
HeWhoPines <30517189+RegionalNightmare@users.noreply.github.com>
2026-02-18 17:58:47 -0500

    Update knexfile to use URL for database connection
    Refactored database connection to use a URL format.

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,8 @@
 require('dotenv').config();
 
-const dbConnection = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    ca: process.env.DB_SSL_CERT,
-    rejectUnauthorized: true
-  }
-};
+const urlDB = `mysql://${process.env.MYQLUSER}:${process.env.MYQLPASSWORD}@${process.env.MYQLHOST}:${process.env.MYQLPORT}/${process.env.MYQLDATABASE}`;
 
-module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: dbConnection,
-    migrations: { directory: './migrations' },
-    seeds: { directory: './seeds' },
-  },
-  production: {
-    client: 'mysql2',
-    connection: dbConnection,
-    migrations: { directory: './migrations' },
-    seeds: { directory: './seeds' },
-  }
-};
+const connection = mysql.createConnection(urlDB);
+
+
+module.exports =  connection;
