b682043b91a8f885367dfcc63247bced7744542a
HeWhoPines <30517189+RegionalNightmare@users.noreply.github.com>
2026-02-18 19:09:17 -0500

    Refactor MySQL connection to use connection pool
    Refactored database connection to use a connection pool with environment variables.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const connection = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,  // Try this first
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-
-const connection = mysql.createConnection(urlDB);
-
-
-module.exports =  connection;
+module.exports = connection;
